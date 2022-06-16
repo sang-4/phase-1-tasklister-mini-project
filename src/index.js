078d01651816ddbdc4701795d2103a8970663ef3
Allan <kipropsallan@gmail.com>
2022-06-16 10:03:07 +0300

    submission

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+const form = document.querySelector("form");
+form.addEventListener("submit", submitForm);
+
+function submitForm(event) {
+  event.preventDefault();
+  console.log(event.target.create_item.value);
+}
 });
